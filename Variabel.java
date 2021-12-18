ab11ce9647e122e6222b98a79e3e2a06b6f5917e
rusdigani <94724259+rusdigani@users.noreply.github.com>
2021-12-18 12:12:14 +0100

    Add files via upload

---
+++ Variabel.java
@@ -0,0 +1,13 @@
+public class Variabel {
+ public static void main(String args[]) {
+	 String salam, nama; 
+	 int tahun; 
+	 salam = "Assalamualaikum";
+	 nama = "Rusdi Abdul Gani";
+	 tahun = 2003;
+	 
+	 System.out.println(salam);
+	 System.out.println("Nama Saya : "+nama);
+	 System.out.println("Saya Lahir di Tahun : "+tahun);
+ }
+}
